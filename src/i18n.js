173c9d3edaf66f9f07dc03012d639ddad5004085
zabak <zabak@Petr-MacBook-Pro-2.local>
2019-04-19 13:47:41 +0200

    Czech translation
    Added Czech translation

--- src/i18n.js
+++ src/i18n.js
@@ -2,12 +2,14 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import de from './locales/de/translation.json';
 import en from './locales/en/translation.json';
+import cs from './locales/cs/translation.json';
 
 
 // Load translations for each language
 const resources = {
   de,
   en,
+  cs,
 };
 
 i18n
